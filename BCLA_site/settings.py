05e1068221fa22c792a27d775894915b94b804cb
suzh <suzhao10086@gmail.com>
2022-01-16 21:24:24 +0800

    修改setting.py

--- BCLA_site/settings.py
+++ BCLA_site/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -28,6 +30,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     'bcla-site-test.herokuapp.com',
     '127.0.0.1',
+    '*'
 ]
 
 
@@ -84,6 +87,9 @@ DATABASES = {
     }
 }
 
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -122,3 +128,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'BCLA_site/static')
+]
+
+django_heroku.settings(locals())
+
+
