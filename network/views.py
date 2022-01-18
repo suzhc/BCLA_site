c9a8bcfec9698e0227893ef4bd14d88faa20d034
suzh <suzhao10086@gmail.com>
2022-01-18 17:32:05 +0800

    若搜索错误则不跳转

--- network/views.py
+++ network/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render_to_response, render
 from django.views.decorators.csrf import csrf_protect, csrf_exempt
 
+import tkinter.messagebox
+from tkinter import *
+
+from django.contrib import messages
+
 from .models import *
 
 from scripts import script
@@ -8,11 +13,10 @@ from scripts import script
 
 # Create your views here.
 def home_page(request):
-    context = {}
-    context['node_list'] = script.node_list()
-    return render_to_response('home_page.html', context)
+    return render_to_response('home_page.html')
 
 
+# 返回
 def return_all_node(request):
     df = script.read_edge_data()
     G = script.convert_to_G(df)
@@ -20,6 +24,7 @@ def return_all_node(request):
     return render_to_response('network.html')
 
 
+# 此函数是用来返回节点自我中心网的可视化页面，后续须做到description页面里去
 def return_node_page(request, node_name):
     df = script.read_edge_data()
     G = script.convert_to_G(df)
@@ -31,9 +36,15 @@ def return_node_page(request, node_name):
 def return_node_description(request):
     if request.method == 'POST':
         searched = request.POST['searched']
+
+        # 这里后改为自己写的函数，用来获取基因信息
         node_descriptions = NetworkNode.objects.filter(node_name__contains=searched).values()
-        return render_to_response('description_page.html', {'searched': searched,
-                                                            'node_descriptions': node_descriptions})
+        if len(node_descriptions) == 0:
+            messages.error(request, 'There is no '+searched)
+            return render(request, 'home_page.html')
+        else:
+            return render_to_response('description_page.html', {'searched': searched,
+                                                                'node_descriptions': node_descriptions})
     else:
         return render_to_response('description_page.html')
 
